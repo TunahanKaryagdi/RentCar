bb199bf3c843db20fbee90a176b89447b75493c6
Tunahan Karyagdi <thankaryagdi@gmail.com>
2022-10-06 21:22:26 +0300

    Initial commit

---
+++ Console/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public class Class1
+    {
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("okey");
+        }
+    }
+}
